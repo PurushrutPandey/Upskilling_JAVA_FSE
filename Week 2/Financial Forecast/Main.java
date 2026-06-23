d841eece6d5717d9b81e1af17f997cdf990c30c6
Purushrut Pandey <purushrut05@gmail.com>
2026-06-23 18:14:47 +0530

    Add Main class for financial forecast calculation

---
+++ Week 2/Financial Forecast/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double presentValue = 10000;
+        double growthRate = 0.08; // 8%
+        int years = 5;
+
+        double futureValue =
+                FinancialForecast.predictFutureValue(
+                        presentValue,
+                        growthRate,
+                        years);
+
+        System.out.println("Present Value: ₹" + presentValue);
+        System.out.println("Growth Rate: " + (growthRate * 100) + "%");
+        System.out.println("Years: " + years);
+        System.out.printf("Predicted Future Value: ₹%.2f%n",
+                futureValue);
+    }
+}
