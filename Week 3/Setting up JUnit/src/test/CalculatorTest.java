e573c9b6ced07936518138d454eea0de02ce88f0
Purushrut Pandey <purushrut05@gmail.com>
2026-07-03 13:01:50 +0530

    Refactor testAddition in CalculatorTest
    Refactor testAddition method to store result in a variable and print it.

--- Week 3/Setting up JUnit/src/test/CalculatorTest.java
+++ Week 3/Setting up JUnit/src/test/CalculatorTest.java
@@ -1,14 +1,19 @@
 package com.example;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+
 public class CalculatorTest {
-     @Test
+
+    @Test
     public void testAddition() {
 
         Calculator calc = new Calculator();
 
-        assertEquals(8, calc.add(5,3));
+        int result = calc.add(5, 3);
 
+        System.out.println("5 + 3 = " + result);
+
+        assertEquals(8, result);
     }
 }
-
